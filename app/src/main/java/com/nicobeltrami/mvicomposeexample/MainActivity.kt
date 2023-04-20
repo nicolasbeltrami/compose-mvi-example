fc02aafbbd3ec14715a623671d04ff370dcc2a33
nicolasbeltrami <nicolas.beltrami@gmail.com>
2023-04-20 00:14:36 -0400

    Testing modal

--- app/src/main/java/com/nicobeltrami/mvicomposeexample/MainActivity.kt
+++ app/src/main/java/com/nicobeltrami/mvicomposeexample/MainActivity.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
@@ -24,8 +27,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nicobeltrami.mvicomposeexample.ui.theme.MVIComposeExampleTheme
 import kotlinx.coroutines.launch
 
@@ -55,15 +60,22 @@ fun BottomSheet() {
 
     ModalBottomSheetLayout(
         sheetState = sheetState,
+        sheetShape = RoundedCornerShape(8.dp),
         sheetContent = {
             Column(
                 modifier = Modifier.padding(16.dp)
             ) {
+                Text(
+                    text = "Título",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
                 repeat(3) { index ->
                     Row(
                         horizontalArrangement = Arrangement.spacedBy(20.dp),
                         modifier = Modifier
-                            .clickable { scope.launch { sheetState.hide() }}
+                            .clickable { scope.launch { sheetState.hide() } }
                             .fillMaxWidth()
                             .padding(vertical = 10.dp)
                     ) {
